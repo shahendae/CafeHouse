c3525cef22b620d437de03a05177f02ad1d410f0
mina <minamedhat_fcis@hotmail.com>
2020-04-13 21:27:29 +0200

    All Orders for Admin Home

--- cafe/src/app/Admin/home-admin/home-admin.component.ts
+++ cafe/src/app/Admin/home-admin/home-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/_models/order';
+import { OrderService } from 'src/app/_service/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-admin',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-admin.component.css']
 })
 export class HomeAdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  orderProducts:Order[] = [];
+  constructor(private orderServ:OrderService, private route:Router) { }
+  
+  Done(id:number){
+      this.orderServ.orderStatus(id).subscribe(a=>{
+      this.orderServ.getOrderProducts().subscribe(o=>{
+        this.orderProducts = o;
+      })
+    });
   }
 
+  ngOnInit(): void {
+    this.orderServ.getOrderProducts().subscribe(o=>{
+      this.orderProducts = o;
+    })
+  }
 }
--- cafe/src/app/_service/order.service.ts
+++ cafe/src/app/_service/order.service.ts
@@ -8,6 +8,7 @@ import { Order } from '../_models/order';
 export class OrderService {
   url = 'http://localhost:56311/api/orders';
   urlOrder = 'http://localhost:56311/api/orders/';
+  urlOrderProducts = 'http://localhost:56311/api/Order/products';
   userId:string = "97910cd0-2bf8-43e0-87b3-96a54381983c";
   constructor(private http: HttpClient) { 
   }
@@ -15,13 +16,20 @@ export class OrderService {
   getOrders(){
     return this.http.get<Order[]>(this.url);
   }
-
   addOrder(order: Order){
     return this.http.post(this.url,order);
   }
 
   getUserOrders(){
     return this.http.get<Order[]>(this.urlOrder+this.userId);
+  }
+
+  getOrderProducts(){
+    return this.http.get<Order[]>(this.urlOrderProducts);
+  }
+
+  orderStatus(orderId:number){
+    return this.http.put<Order>(this.url+'/'+orderId,null);
   }
 
 }
