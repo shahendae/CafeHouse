6690cf52e279ddce0a5e135b32ac1654e8491805
Abanob-Hanna <54998812+Abanob-Hanna@users.noreply.github.com>
2020-04-13 21:12:58 +0200

    My Orders

--- cafe/src/app/User/my-orders/my-orders.component.ts
+++ cafe/src/app/User/my-orders/my-orders.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { OrderService } from 'src/app/_service/order.service';
 import { Order } from 'src/app/_models/order';
-import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-my-orders',
@@ -17,43 +17,29 @@ export class MyOrdersComponent implements OnInit {
 
 
   fromDate : Date ;
-  datePipe:DatePipe ;
   toDate:Date ;
   GetData(date){
-    this.fromDate = new Date(date.value.fromDate);
-    this.toDate = new Date(date.value.toDate) ; 
-    console.log(this.fromDate);
-    console.log(this.toDate);
-    //this.datePipe.transform(this.fromDate,"M/d/yy");
-    
-    
-    var date1 = this.datePipe.transform(this.fromDate,"DD/MM/Y");
-    console.log(date1);
+    this.fromDate = date.value.fromDate;
+    this.toDate = date.value.toDate ; 
+    this.s.getUserOrders(this.fromDate,this.toDate).subscribe(o => {
+        
+      this.myOrders = o;
 
-
-    
-    
- 
-
+      
+    })
   }
-  
-  
-  // Testing(){
-  //   this.myOrders.splice(this.myOrders.length-2,this.myOrders.length-1);
-  // }
-  
+  Show(data){
+    alert(data);
+  }
   
     myOrders : Order [] = [];
     constructor(private s:OrderService) { }
   
+  
     
     ngOnInit(): void {
-      this.s.getUserOrders().subscribe(o => {
-        
-        this.myOrders = o;
-  
-        
-      })
+      
+      
     }
 
 }
--- cafe/src/app/_service/order.service.ts
+++ cafe/src/app/_service/order.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Order } from '../_models/order';
+import {UserService} from '../_service/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   url = 'http://localhost:56311/api/orders';
-  urlOrder = 'http://localhost:56311/api/orders/';
-  userId:string = "97910cd0-2bf8-43e0-87b3-96a54381983c";
-  constructor(private http: HttpClient) { 
+  urlOrder = 'http://localhost:56311/api/order/';
+  userId = this.userserve.CurrentUserId();
+  
+  
+  constructor(private http: HttpClient , private userserve : UserService) { 
   }
 
   getOrders(){
@@ -20,8 +23,8 @@ export class OrderService {
     return this.http.post(this.url,order);
   }
 
-  getUserOrders(){
-    return this.http.get<Order[]>(this.urlOrder+this.userId);
+  getUserOrders(from:Date , to:Date){
+    return this.http.get<Order[]>(this.urlOrder+this.userId+"/"+from+"/"+to);
   }
 
 }
